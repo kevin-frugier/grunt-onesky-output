5202836b04b293626ed95d3cb7790b3dbaddfcc3
Kevin Frugier <kevin@speakplus.fr>
2015-03-17 12:24:26 +0100

    Indent + added possibility to set credentials from env variables + locale file merged with content of all components fount (not just the last one) + removed undocumented JSON magic (until understood)

--- tasks/onesky.js
+++ tasks/onesky.js
@@ -8,64 +8,81 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  var Onesky = require('onesky')('', '');
-
-
-  /**
-   * @returns {Object} returns an object containing a publickey and privatekey
-   */
-  function getCredentials() {
-    if (grunt.file.exists('onesky.json')) {
-      return JSON.parse(grunt.file.read('onesky.json'));
-    } else {
-      grunt.fail.warn("onesky.json file not found");
-    }
-  }
-
-  
-  grunt.registerMultiTask('oneskyoutput', 'Pull translation from onesky', function() {
-    var self = this;
-    var credentials = getCredentials();
-    var onesky = require('onesky')(credentials.publickey, credentials.privatekey);
-    var path = path = (self.data.path === undefined) ? './tmp/lang' : this.data.path;
-    var done = this.async();
-    var keys,langKeys,filename,fjson;
-
-    onesky.string.output({
-      platformId: this.data.platformId,
-      locale: this.data.locale
-    }, function(a,data) {
-      keys = Object.keys(data.translation);
-      for(var i=0; i<keys.length; i++) {
-        langKeys = Object.keys(data.translation[keys[i]]);
-        for(var y=0; y<langKeys.length; y++) {
-          filename = path+'/'+langKeys[y]+".json";
-          fjson = JSON.stringify(data.translation[keys[i]][langKeys[y]]);
-          fjson = JSON.parse(fjson, function(key,value){
-          	if (value && typeof value === 'object') {
-
-      			for (var k in value) {
-        			if (/^[a-zA-Z."-_]/.test(k) && Object.hasOwnProperty.call(value, k)) {
-        				value[k.replace(/\"/g, '')] = value[k];
-          				delete value[k];
-        			}
-      			}
-      		}
-      		
-      		return value;
-
-          });
-          grunt.file.write(filename, JSON.stringify(fjson));
-          grunt.log.writeln('File '+filename+' written.');
+    /**
+     * @returns {Object} returns an object containing a publickey and privatekey
+     */
+    function getCredentials() {
+        if (grunt.file.exists('onesky.json')) {
+            return JSON.parse(grunt.file.read('onesky.json'));
+        } else {
+            var credentials = {
+                publickey: process.env.ONESKY_PUBLICKEY || null,
+                privatekey: process.env.ONESKY_PRIVATEKEY || null
+            };
+            if (!credentials.publickey || !credentials.privatekey) {
+                grunt.fail.warn("onesky.json file not found and missing env variables ONESKY_PUBLICKEY or ONESKY_PRIVATEKEY");
+            } else {
+                return credentials;
+            }
         }
-        
-      }
-      
-      done();
-    });
+    }
 
-  });
+
+    grunt.registerMultiTask('oneskyoutput', 'Pull translation from onesky', function () {
+        var self = this;
+        var credentials = getCredentials();
+        var onesky = require('onesky')(credentials.publickey, credentials.privatekey);
+        var path = path = (self.data.path === undefined) ? './tmp/lang' : this.data.path;
+        var done = this.async();
+
+        onesky.string.output({
+            platformId: this.data.platformId,
+            locale: this.data.locale
+        }, function (err, oneSkyData) {
+
+            var componentKeys = Object.keys(oneSkyData.translation);
+            var _locales, _locale, _content, _localeData;
+            var localesData = {};
+
+            for (var iComponent = 0; iComponent < componentKeys.length; iComponent++) {
+                _locales = Object.keys(oneSkyData.translation[componentKeys[iComponent]]);
+
+                for (var iLocale = 0; iLocale < _locales.length; iLocale++) {
+                    _locale = _locales[iLocale];
+
+                    if (!localesData.hasOwnProperty(_locale)) {
+                        localesData[_locale] = {
+                            content: {},
+                            filename: path + '/' + _locale + ".json"
+                        };
+                    }
+                    _localeData = localesData[_locale];
+
+                    _content = oneSkyData.translation[componentKeys[iComponent]][_locale];
+                    for (var _key in _content) {
+                        if (_content.hasOwnProperty(_key)) {
+                            if (_localeData.content.hasOwnProperty(_key)) {
+                                console.log('Key overwritten : ' + _key);
+                            }
+                            _localeData.content[_key] = _content[_key];
+                        }
+                    }
+                }
+            }
+
+            for (_locale in localesData) {
+                _localeData = localesData[_locale];
+                grunt.file.write(_localeData.filename, JSON.stringify(_localeData.content, null, 4));
+                grunt.log.writeln('File ' + _localeData.filename + ' written.');
+            }
+
+
+
+            done();
+        });
+
+    });
 
 };
